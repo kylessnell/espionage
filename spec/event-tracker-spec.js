b07a45ea2547d87633ab98f770ed6a32debf2da4
kylessnell <kylessnell@gmail.com>
2013-05-14 14:05:23 -0400

    First Commit

--- spec/event-tracker-spec.js
+++ spec/event-tracker-spec.js
@@ -1,6 +1,7 @@
 beforeEach(function() {
   $("<a href='#' data-track='next-brand-button'></a>").appendTo('body');
   $("<script> var _gaq = _gaq || []; </script>").appendTo('body');
+
   this.addMatchers({
     toBeTracked: function(trackingLabel) {
       var isTracked = _gaq[0] == ['_trackEvent', trackingLabel, 'click'];
