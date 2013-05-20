0f5ac33013ed1f8c32bc18083b93a177128ca562
kylessnell <kylessnell@gmail.com>
2013-05-20 10:20:26 -0400

    updated tests

--- spec/espionage-spec.js
+++ spec/espionage-spec.js
@@ -1,19 +1,11 @@
 beforeEach(function() {
-  $("<a href='#' data-track='next-brand-button'></a>").appendTo('body');
+  $("<a href='#' id='click-me' data-track='next-brand-button'></a>").appendTo('body');
   $("<script> var _gaq = _gaq || []; </script>").appendTo('body');
-
-  this.addMatchers({
-    toBeTracked: function(trackingLabel) {
-      var isTracked = _gaq[0] == ['_trackEvent', trackingLabel, 'click'];
-    }
-  });
 });
 
-describe('googleAnalyticsTrackClick', function() {
-  it('adds events to the GA event tracking queue when elements with a data-track attribute are clicked', function() {
-   var trackableLink = $("a[data-track='next-brand-button']");
-   trackableLink.on('click', function() {
-     expect($(trackableLink)).toBeTracked('next-brand-button');
-   });
+describe('clicking an anchor with a data-track attributes', function() {
+  it('adds the click event to the GA event tracking queue', function() {
+    $('a[data-track]').trigger('click');
+    expect(_gaq[0]).toEqual(['_trackEvent', 'next-brand-button', 'click']);
   });
 });
